2e438d40d22abba499a3d0be9e8e271d6f44b454
AF86975-005Do0000020g79IAA <139264557+AF86975-005Do0000020g79IAA@users.noreply.github.com>
2023-07-13 10:23:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner,Breakfast Menu')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Omeza3 & oat meal')
+streamlit.text('kult & spinach ,rocket smoothly')
+streamlit.text('Hard-boiled ,free-range egg')
