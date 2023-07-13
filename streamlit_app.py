1e9bfd96c3d33fef0e1f77b54d9a87f4e4688c82
AF86975-005Do0000020g79IAA <139264557+AF86975-005Do0000020g79IAA@users.noreply.github.com>
2023-07-13 12:02:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ streamlit.text('🥣,Omeza3 & oat meal')
 streamlit.text('🥗,kult & spinach ,rocket smoothly')
 streamlit.text('🐔,Hard-boiled ,free-range egg')
 streamlit.text('🥑🍞,Avcado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
