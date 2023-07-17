fbd3f4aade0282c85e225039e7bd8a2fd413a251
AF86975-005Do0000020g79IAA <139264557+AF86975-005Do0000020g79IAA@users.noreply.github.com>
2023-07-17 18:58:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,4 @@ streamlit.text('🥑🍞,Avcado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocada','strawberries'])
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-fruits_to_show =  my_fruit_list.loc[fruits_selected]
-
 
